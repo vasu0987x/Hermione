73aa27452c8aa0e4dd158a27240e84946fda1689
vasu0987x <vasupareek2x4@gmail.com>
2025-02-05 20:57:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,10 +61,8 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'USE FREE')
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Vasu:%40Vasu2412@cluster0.2ajb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://Vasu:%40Vasu2412@cluster0.2ajb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Vasu:%40Vasu2412@cluster0.2ajb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://Vasu:%40Vasu2412@cluster0.2ajb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "vasu")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deendayal_files')
 
