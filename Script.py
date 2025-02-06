5cc91d04889a8411293dc1b0eee749a4f00f8a9d
vasu0987x <vasupareek2x4@gmail.com>
2025-02-06 08:54:45 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -271,7 +271,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/Deendayal_dhakad">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href=https://t.me/Deendayal_dhakad>[Deendayal_Dhakad]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/BhediyaTheater">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href=https://t.me/BhediyaTheater>[Bhediya Theater]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
